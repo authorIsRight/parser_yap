d810665d840a4ced9f04eb513b2f1faaae29aab1
Илья Соболев <i4sobolev@yandex.ru>
2023-03-08 19:09:19 +0100

    before project

--- configs.py
+++ configs.py
@@ -1,5 +1,16 @@
-# configs.py 
+# configs.py
 import argparse
+from constants import BASE_DIR
+# модуль для работы с логами,
+import logging
+# хендлер с ротацией логов.
+from logging.handlers import RotatingFileHandler
+
+# Описание формата логов: 
+# Время записи – Уровень сообщения – Cообщение.
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+# Указываем формат времени.
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
 
 def configure_argument_parser(available_modes):
     parser = argparse.ArgumentParser(description='Парсер документации Python')
@@ -13,5 +24,39 @@ def configure_argument_parser(available_modes):
         '--clear-cache',
         action='store_true',
         help='Очистка кеша'
-    )    
+    )
+    # Новый аргумент.
+    # Новый аргумент --output вместо аргумента --pretty
+    parser.add_argument(
+        '-o',
+        '--output',
+        choices=('pretty', 'file'),
+        help='Дополнительные способы вывода данных'
+    )
     return parser 
+
+# Добавьте новую функцию и напишите нужные команды.
+def configure_logging():
+    # Сформируйте путь до директории logs.
+    log_dir = BASE_DIR/ 'logs'
+    # Создайте директорию.
+    log_dir.mkdir(exist_ok=True)
+    # Отсюда начинается новый код!
+    # Получение абсолютного пути до файла с логами.
+    log_file = log_dir / 'parser.log'
+    
+    # Инициализация хендлера с ротацией логов.
+    # Максимальный объём одного файла — десять в шестой степени байт (10**6), 
+    # максимальное количество файлов с логами — 5.
+    rotating_handler = RotatingFileHandler(
+        log_file, maxBytes=10 ** 6, backupCount=5, encoding='utf-8'
+    )
+    # Базовая настройка логирования basicConfig.
+    logging.basicConfig(
+        datefmt=DT_FORMAT,
+        format=LOG_FORMAT,
+        # Уровень записи логов.
+        level=logging.INFO,
+        # Вывод логов в терминал.
+        handlers=(rotating_handler, logging.StreamHandler())
+    )     
--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 # constants.py
 from pathlib import Path
+from datetime import datetime
 
 MAIN_DOC_URL = 'https://docs.python.org/3/'
 BASE_DIR = Path(__file__).parent 
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
